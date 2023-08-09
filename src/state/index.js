e2318cf7966190b149e36eb792d9b3bf70dc201f
yasmineRajhi <91615698+yasmineRajhi@users.noreply.github.com>
2023-08-09 10:39:57 +0100

    front

---
+++ src/state/index.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  events: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setEvents: (state, action) => {
+      state.events = action.payload.events;
+    },
+    setEvent: (state, action) => {
+      const updatedEvents = state.events.map((event) => {
+        if (event._id === action.payload.event_id) return action.payload.events;
+        return event;
+      });
+      state.events = updatedEvents;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setEvents, setEvent } =
+  authSlice.actions;
+export default authSlice.reducer;
